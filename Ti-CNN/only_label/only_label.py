3367d2e6d827b9e96ad646c5a42679b6f242b7f6
Color <colorful-black@outlook.com>
2024-04-08 22:51:04 +0800

    添加了Ti-CNN的only_label代码

---
+++ Ti-CNN/only_label/only_label.py
@@ -0,0 +1,22 @@
+import json
+import random
+
+read_path = 'D:\学习\毕设\数据\数据整理\Ti-CNN\原始数据\Ti-CNN_total_data_analysis.json'
+write_path = 'TI-CNN_only_label'
+total_data = []
+
+with open(read_path,'r',encoding='utf-8') as f:
+    data = json.load(f)
+    for news in data:
+        conversation = {}
+        label = news['label']
+        answer ='label: ' + label
+        input = 'time: '+news['time'] +' \n '+'author: '+ news['author'] +' \n '+'source: ' + news['url'] +' \n '+'text: ' + news['text']
+        conversation[
+            'instruction'] = "You will be responsible for explaining the veracity of a particular news story. During this process, you will receive a news text material. Your task is to provide the label of the news without any explanation. \nYour answer should follow the following format: Label: real/fake"
+        conversation['input'] = input
+        conversation['output'] = answer
+        total_data.append(conversation)
+    random.shuffle(total_data)
+with open(write_path,'w',encoding='utf-8') as f:
+    json.dump(total_data,f,ensure_ascii=False,indent=4)
