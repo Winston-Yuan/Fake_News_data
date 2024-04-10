f11434a3544ec246c7db53b8f4d862322c036d67
Color <colorful-black@outlook.com>
2024-04-10 21:08:17 +0800

    完成了Weibo数据集的GPT补充

---
+++ 模型回答/Ti-CNN_explanation_label/train_2024-04-09-19-18-43_Ti-CNN_explanation_label/准确率检测_CHECKED.py
@@ -0,0 +1,34 @@
+import json
+
+true = 0  # 判断正确
+false = 0  # 判断错误
+wrong = 0  # 没给出判断
+total = 0
+false_ID = []
+wrong_ID = []
+with open('answer_CHECKED_explanation_label.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+    for news_key in data:
+        news = data[news_key]
+        model_answer = news['model_answer']
+        model_answer_list = model_answer.split('label: ')
+        output = news['output']
+        output_list = output.split('label: ')
+        print(news_key,output_list[-1],model_answer_list[-1])
+        if model_answer_list[-1] == output_list[-1]:
+            print(1)
+            true += 1
+        elif (model_answer_list[-1] != 'fake')&(model_answer_list[-1] != 'real'):
+            print(2)
+            wrong += 1
+            wrong_ID.append(news_key)
+        elif model_answer_list[-1] != output_list[-1]:
+            print(0)
+            false += 1
+            false_ID.append(news_key)
+        total += 1
+print(true / (total-wrong))#打印剔除了偏答的答案后的准确率
+print(true / total)#打印未剔除偏答的答案后的准确率
+print('false_ID',false_ID)
+print('wrong_ID',wrong_ID)
+
