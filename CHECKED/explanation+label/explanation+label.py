d5e9e9f2d1fde35dc013e28fafe873a490dac5ea
Color <colorful-black@outlook.com>
2024-04-11 15:32:23 +0800

    修改了所有的prompt，使模型能够分辨prompt和新闻内容

--- CHECKED/explanation+label/explanation+label.py
+++ CHECKED/explanation+label/explanation+label.py
@@ -22,7 +22,7 @@ with open(read_path,'r',encoding='utf-8') as f:
         else:
             author = 'null'
         input = 'time: '+news['time'] +' \n '+'author: '+ author +' \n '+'source: ' + news['url'] +' \n '+'text: ' + news['text']
-        conversation['instruction'] ="You will be responsible for explaining the veracity of a particular news story. During this process, you will receive a news text material. Your task is to provide a detailed analysis of the veracity of the news and a reasonable explanation to clarify the basis for determining whether each report is true or false. \nYour answer should follow the following format:\nLet's think step by step\nSource reliability: XXX\nAuthor background: XXX\nEvidence test: XXX\nLanguage style: XXX\nOther analysis: XXX\nLabel: real/fake"
+        conversation['instruction'] ="You will be responsible for explaining the veracity of a particular news story. During this process, you will receive a news text material. Your task is to provide a detailed analysis of the veracity of the news and a reasonable explanation to clarify the basis for determining whether each report is true or false. \nYour answer should follow the following format:\nSource reliability: XXX\nAuthor background: XXX\nEvidence test: XXX\nLanguage style: XXX\nOther analysis: XXX\nLabel: real/fake \nHere is the news text material:"
         conversation['input'] = input
         conversation['output'] = answer
         total_data.append(conversation)
